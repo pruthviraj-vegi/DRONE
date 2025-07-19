847dc4e906674b652c4271a4e117b339676fec87
Clown-Bunny <59609113+pruthviraj-vegi@users.noreply.github.com>
2025-07-19 22:15:15 +0530

    trusted origin

--- Drone/settings.py
+++ Drone/settings.py
@@ -27,8 +27,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
-CSRF_TRUSTED_ORIGINS = ["https://drone.clownbunny.in"]
-
 AUTH_USER_MODEL = "users.CustomUser"
 
 LOGIN_URL = "base:login"
@@ -136,6 +134,11 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = ["https://bill.waayuputra.co.in"]
+CSRF_TRUSTED_ORIGINS = [
+    "https://bill.waayuputra.co.in",
+]
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
